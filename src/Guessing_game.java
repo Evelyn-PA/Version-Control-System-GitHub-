4ac13c731e9e28c87e6fe8330b6dad55f0941ff1
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2023-10-01 16:29:50 +0700

    Add the comment for the Guessing_game program.
    I added the comment for the Guessing_game program to make the viewers read the code more easily.

--- src/Guessing_game.java
+++ src/Guessing_game.java
@@ -6,21 +6,22 @@ public class Guessing_game {
 		Scanner in = new Scanner(System.in);
 		int i = 0;
 		String answer = "Anhnguyen";
+		//Make a loop to ask the players guess the name
 		while (true) {
 			System.out.println("Please, guess my name.");
 
 			String guess = in.nextLine();
 			i++;
-			
-			if (guess.equals(answer)) {
+			//If the players guess the right answer -> print "Congratulations!" then exit the program
+			if (guess.equals(answer)) { 
 				System.out.println("Congratulations!");
 				System.out.println("Guesses: " + i );
 				break;
 			}
-			
+			//Ask players to continue or exit the program
 			System.out.println("Do you want to quit (y/n) ?");
 			String ans = in.nextLine();
-			
+			//if the players exit the game -> print how many times they guess and exit
 			 if (ans.equals("y")) {
 				
 				System.out.println("Guesses: " + i );
